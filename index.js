692e91f91056793892fb5a9ebd8a5e955aa321a4
Kirill <93333934+Povasin@users.noreply.github.com>
2022-12-16 17:23:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ const http = require('http');
 const bcrypt = require('bcrypt');
 app.use(express.static(path.resolve(__dirname, './')));
 app.use(express.json()) 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+  );
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
+});
+
 const Datastore = require('nedb');
 const users = new Datastore({filename : './server/orders', autoload: true });
 users.loadDatabase();
